a7c25f1ec6829901fe91502f8148af6361a14341
mauherrerag <mauherrerag@email.arizona.edu>
2019-11-22 13:44:03 -0700

    modified comments for constants

--- src/characters/Aliens/Enemy.java
+++ src/characters/Aliens/Enemy.java
@@ -5,32 +5,32 @@ import characters.BoardCharacter;
 public class Enemy extends BoardCharacter{
 	// Default values for characters
 	
-	// AstroJoe
+	// LittleGreenMen
 	protected static final int HEALTH_LITTLE_GREEN_MEN = 100;
 	protected static final int ATTACK_SPEED_LITTLE_GREEN_MEN = 100;	
 	protected static final int DAMAGE_LITTLE_GREEN_MEN = 100;
 	
-	// LoadedAstroJoe
+	// Grunt
 	protected static final int HEALTH_GRUNT = 100;
 	protected static final int ATTACK_SPEED_GRUNT = 100;
 	protected static final int DAMAGE_GRUNT = 100;
 	
-	// SpaceBucksPrinter
+	// Sprinter
 	protected static final int HEALTH_SPRINTER = 100;
 	protected static final int ATTACK_SPEED_SPRINTER = 100;
 	protected static final int DAMAGE_SPRINTER = 100;
 	
-	// SpaceBucksFactory
+	// Tank
 	protected static final int HEALTH_TANK = 100;
 	protected static final int ATTACK_SPEED_TANK = 100;
 	protected static final int DAMAGE_TANK = 100;
 	
-	// StartrellCluggins
+	// ManHunter
 	protected static final int HEALTH_MAN_HUNTER = 100;
 	protected static final int ATTACK_SPEED_MAN_HUNTER = 100;
 	protected static final int DAMAGE_MAN_HUNTER = 100;
 	
-	// Asteroid
+	// Gargantua
 	protected static final int HEALTH_GARGANTUA = 100;
 	protected static final int ATTACK_SPEED_GARGANTUA = 100;
 	protected static final int DAMAGE_GARGANTUA = 100;
