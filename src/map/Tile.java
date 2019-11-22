57555f3243921fc847b3234650bf93927964a2ec
mauherrerag <mauherrerag@email.arizona.edu>
2019-11-22 13:47:36 -0700

    created stub methods

--- src/map/Tile.java
+++ src/map/Tile.java
@@ -1,5 +1,28 @@
 package map;
 
+import characters.BoardCharacter;
+
 public class Tile {
+	// Class fields
+	private boolean isOccupied;
+	private BoardCharacter characterAtTile;
+	
+	// Constructor
+	public Tile() {
+		
+	}
+	
+	// Methods
+	public boolean isOccupied() {
+		return isOccupied;
+	}
+	
+	public BoardCharacter getCharacter() {
+		return characterAtTile;
+	}
+	
+	public void placeCharacter(BoardCharacter character) {
+		this.characterAtTile = character;
+	}
 
 }
