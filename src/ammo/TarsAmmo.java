bd20b29a6a227b6db3896a54913cc3efd2e8ed57
Timothy <timothylukau@email.arizona.edu>
2019-12-02 21:37:23 -0700

    Tars Ammo class

---
+++ src/ammo/TarsAmmo.java
@@ -0,0 +1,14 @@
+package ammo;
+
+import characters.Astronauts.DefenderTower;
+import characters.Astronauts.Tars;
+import javafx.scene.image.Image;
+
+public class TarsAmmo extends Ammo{
+
+	public TarsAmmo(DefenderTower dt, Image sprite) {
+		super(new Tars(), new Image(TARS_AMMO_SPRITE, SPRITE_WIDTH, SPRITE_HEIGHT, false, false));
+		// TODO Auto-generated constructor stub
+	}
+
+}
