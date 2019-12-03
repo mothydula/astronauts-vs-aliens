fadc62ba452262a985f38a48c2f9571032e80f2a
Timothy <timothylukau@email.arizona.edu>
2019-12-02 21:21:40 -0700

    Adding constants to ammo class

--- src/ammo/Ammo.java
+++ src/ammo/Ammo.java
@@ -7,6 +7,12 @@ public class Ammo {
 	private int damage;
 	private int speed;
 	private Image sprite;
+	
+	protected static final int SPRITE_WIDTH = 35;
+	protected static final int SPRITE_HEIGHT = 35;
+	//AstroJoe Ammo Image
+	public static final String ASTROJOE_AMMO_SPRITE = 
+	
 	public Ammo (DefenderTower dt, Image sprite) {
 		this.speed = dt.getAttackSpeed();
 		this.damage = dt.getDamage();
