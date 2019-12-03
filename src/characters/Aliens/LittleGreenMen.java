b24ced3f07495d9b61f3be8dde5c09b553c6c485
Mauricio Herrera <mauherrerag@email.arizona.edu>
2019-12-02 20:37:26 -0700

    changed visibility to public for constructor

--- src/characters/Aliens/LittleGreenMen.java
+++ src/characters/Aliens/LittleGreenMen.java
@@ -4,7 +4,7 @@ import javafx.scene.image.Image;
 
 public class LittleGreenMen extends Enemy{
 
-	protected LittleGreenMen() {
+	public LittleGreenMen() {
 		super(HEALTH_LITTLE_GREEN_MEN, ATTACK_SPEED_LITTLE_GREEN_MEN, 
 				DAMAGE_LITTLE_GREEN_MEN, new Image(LITTLEGREENMEN_IMAGE, 50, 50, false, false));
 	}
