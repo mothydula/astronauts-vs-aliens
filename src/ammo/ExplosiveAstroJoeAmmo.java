4f47f2c25a1ce354cf9761821145ef26c55121e4
Timothy <timothylukau@email.arizona.edu>
2019-12-02 21:31:10 -0700

    Explosive Astro Joe ammo class

---
+++ src/ammo/ExplosiveAstroJoeAmmo.java
@@ -0,0 +1,14 @@
+package ammo;
+
+import characters.Astronauts.DefenderTower;
+import characters.Astronauts.ExplosiveAstroJoe;
+import javafx.scene.image.Image;
+
+public class ExplosiveAstroJoeAmmo extends Ammo{
+
+	public ExplosiveAstroJoeAmmo(DefenderTower dt, Image sprite) {
+		super(new ExplosiveAstroJoe(), new Image(EXPLOSIVE_ASTROJOE_AMMO_SPRITE, SPRITE_WIDTH, SPRITE_HEIGHT, false, false));
+		// TODO Auto-generated constructor stub
+	}
+
+}
