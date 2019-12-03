18acf7cd8e10e92346c5eadd61c8dc1d63678904
Timothy <timothylukau@email.arizona.edu>
2019-12-02 21:39:48 -0700

    Commenting ExplosiveAstroJoe's Ammo class

--- src/ammo/ExplosiveAstroJoeAmmo.java
+++ src/ammo/ExplosiveAstroJoeAmmo.java
@@ -3,9 +3,11 @@ package ammo;
 import characters.Astronauts.DefenderTower;
 import characters.Astronauts.ExplosiveAstroJoe;
 import javafx.scene.image.Image;
-
+/**
+ * ExplosiveAstroJoe's Ammo class
+ * */
 public class ExplosiveAstroJoeAmmo extends Ammo{
-
+	
 	public ExplosiveAstroJoeAmmo(DefenderTower dt, Image sprite) {
 		super(new ExplosiveAstroJoe(), new Image(EXPLOSIVE_ASTROJOE_AMMO_SPRITE, SPRITE_WIDTH, SPRITE_HEIGHT, false, false));
 		// TODO Auto-generated constructor stub
