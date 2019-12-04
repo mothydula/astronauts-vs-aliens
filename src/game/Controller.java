a9f3eecefb8c62052d3e6df335d219d1fe0bb5ee
Mauricio Herrera <mauherrerag@email.arizona.edu>
2019-12-03 23:37:58 -0700

    implemented game timer, and enemy waves

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -4,10 +4,13 @@ package game;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import characters.BoardCharacter;
-import characters.Aliens.Enemy;
-import characters.Aliens.LittleGreenMen;
+import characters.Aliens.*;
 import characters.Astronauts.DefenderTower;
 import characters.IncomeTowers.IncomeTower;
 import javafx.animation.KeyFrame;
@@ -21,14 +24,19 @@ public class Controller {
 	public static final int COLS = 12;
 	private Model model;
 	private static final long FRAME_TIME = 100; // Milliseconds
-	private static final int STAGE_ONE_ALIENS = 15;
-	private static final int STAGE_TWO_ALIENS = 30;
-	private static final int STAGE_THREE_ALIENS = 40;
+	private static final int WAVE_ONE_ALIENS = 15;
+	private static final int WAVE_TWO_ALIENS = 30;
+	private static final int WAVE_THREE_ALIENS = 40;
 	private int currentIncome;
 	private final int CURRENCY_TIMELINE = 10; // seconds
 	private final int CURRENCY_DEPOSIT = 25;
 	private Timeline moneyTimeline;
 	private Timeline alienTimeline;
+	private static Random rand;
+	private static final int RANDOM_COLUMN_BOUND = 3;
+	private final long WAVE_DELAY = 30000l;
+	private AtomicLong timeElapsed;
+	private Timer gameTimer;
 
 	
 	// Constructor
@@ -38,12 +46,46 @@ public class Controller {
 	
 	// Methods
 	public void initialize() {
+		timeElapsed = new AtomicLong(0);
+		startGameTimer();
+		rand = new Random();
 		model.depositSpacebucks(50);
 		currentIncome = 0;
 		model.setSpeedMultiplier(1);
 		generateAliens();
 		new Thread(() -> startAlienTimeline()).start();
 		startMoneyTimeline();
+	}
+	
+	private void startGameTimer() {
+		gameTimer = new Timer();
+		TimerTask increaseTime = new TimerTask() {
+
+			@Override
+			public void run() {
+				long newValue = timeElapsed.get() + 1 * model.getSpeedMultiplier();
+				timeElapsed.set(newValue);
+			}
+			
+		};
+		gameTimer.schedule(increaseTime, 0, 1);
+	}
+	
+	private void pauseGameTimer() {
+		gameTimer.cancel();
+	}
+	
+	private void resumeGameTimer() {
+		TimerTask increaseTime = new TimerTask() {
+
+			@Override
+			public void run() {
+				long newValue = timeElapsed.get() + 1 * model.getSpeedMultiplier();
+				timeElapsed.set(newValue);
+			}
+			
+		};
+		gameTimer.schedule(increaseTime, 0, 1);
 	}
 	
 	private void startMoneyTimeline() {
@@ -60,6 +102,10 @@ public class Controller {
 		alienTimeline = new Timeline(new KeyFrame(Duration.millis(FRAME_TIME / model.getSpeedMultiplier()), e -> {
 			if (model.hasAliens()) {
 				animate();
+			}else {
+				System.out.println("NO MORE ALIENS");
+				model.setWaveNumber(model.getWaveNumber()+1);
+				generateAliens();
 			}
 		}));
 		
@@ -68,34 +114,167 @@ public class Controller {
 	}
 	
 	private void generateAliens() {
-		Random rand = new Random();
+		Timer timer = new Timer();
 		int waveNumber = model.getWaveNumber();
 		
-		if (waveNumber == 1) {
-			for (int i = 0; i < STAGE_ONE_ALIENS; i++) { 
-				LittleGreenMen alien = new LittleGreenMen();
-				int row = rand.nextInt(ROWS);
-				int col = rand.nextInt(6) + COLS + 2;
-				alien.setRow(row);
-				alien.setCol(col);
-				alien.setStackPane();
+		if (waveNumber == 1) {						// WAVE ONE
+			System.out.println("WAVE ONE");
+			TimerTask firstWave = new TimerTask() {
+
+				@Override
+				public void run() {
+					generateLittleGreenMan(3);
+					generateGrunt(3);
+				}
 				
-				model.addAlien(alien);
-				Platform.runLater(() -> model.placeCharacter(alien, row, col));
-			}
-		} else if (waveNumber == 2) {
+			};
 			
-		} else if (waveNumber == 3) {
+			TimerTask secondWave = new TimerTask() {
+
+				@Override
+				public void run() {
+					generateLittleGreenMan(3);
+					generateGrunt(3);
+					generateSprinter(3);
+				}
+				
+			};
+			timer.schedule(firstWave, 0);
+			timer.schedule(secondWave, WAVE_DELAY);
+		} else if (waveNumber == 2) {				// WAVE TWO
+			System.out.println("WAVE TWO");
+
+			TimerTask firstWave = new TimerTask() {
+
+				@Override
+				public void run() {
+					generateLittleGreenMan(6);
+					generateGrunt(6);
+					generateSprinter(3);
+					generateManHunter(3);
+					generateTank(3);
+				}
+				
+			};
+			
+			TimerTask secondWave = new TimerTask() {
+
+				@Override
+				public void run() {
+					generateTank(3);
+					generateManHunter(3);
+				}
+				
+			};
+			timer.schedule(firstWave, WAVE_DELAY);
+			timer.schedule(secondWave, WAVE_DELAY/3);
+			
+		} else if (waveNumber == 3) {				// WAVE THREE
+			System.out.println("WAVE THREE");
+			
+			TimerTask firstWave = new TimerTask() {
+
+				@Override
+				public void run() {
+					generateLittleGreenMan(6);
+					generateGrunt(6);
+					generateSprinter(6);
+					generateManHunter(6);
+					generateTank(3);
+					generateGargantua(3);
+				}
+				
+			};
+			
+			TimerTask secondWave = new TimerTask() {
+
+				@Override
+				public void run() {
+					
+				}
+				
+			};
+			timer.schedule(firstWave, WAVE_DELAY);
+			timer.schedule(secondWave, WAVE_DELAY);
 			
 		}
 	}
 	
-	private void generateWaveOne() {
-		Random rand = new Random();
-		for (int i = 0; i < 6; i++) {
+	private void generateLittleGreenMan(int amount) {
+		for (int i = 0; i < amount; i++) {
 			LittleGreenMen alien = new LittleGreenMen();
 			int row = rand.nextInt(ROWS);
-			int col = rand.nextInt(3) + COLS+2;
+			int col = rand.nextInt(RANDOM_COLUMN_BOUND) + COLS + 2;
+			alien.setRow(row);
+			alien.setCol(col);
+			alien.setStackPane();
+			
+			model.addAlien(alien);
+			Platform.runLater(() -> model.placeCharacter(alien, row, col));
+		}
+	}
+	
+	private void generateSprinter(int amount) {
+		for (int i = 0; i < amount; i++) {
+			Sprinter alien = new Sprinter();
+			int row = rand.nextInt(ROWS);
+			int col = rand.nextInt(RANDOM_COLUMN_BOUND) + COLS + 2;
+			alien.setRow(row);
+			alien.setCol(col);
+			alien.setStackPane();
+			
+			model.addAlien(alien);
+			Platform.runLater(() -> model.placeCharacter(alien, row, col));
+		}
+	}
+	
+	private void generateGrunt(int amount) {
+		for (int i = 0; i < amount; i++) {
+			Grunt alien = new Grunt();
+			int row = rand.nextInt(ROWS);
+			int col = rand.nextInt(RANDOM_COLUMN_BOUND) + COLS + 2;
+			alien.setRow(row);
+			alien.setCol(col);
+			alien.setStackPane();
+			
+			model.addAlien(alien);
+			Platform.runLater(() -> model.placeCharacter(alien, row, col));
+		}
+	}
+	
+	private void generateManHunter(int amount) {
+		for (int i = 0; i < amount; i++) {
+			ManHunter alien = new ManHunter();
+			int row = rand.nextInt(ROWS);
+			int col = rand.nextInt(RANDOM_COLUMN_BOUND) + COLS + 2;
+			alien.setRow(row);
+			alien.setCol(col);
+			alien.setStackPane();
+			
+			model.addAlien(alien);
+			Platform.runLater(() -> model.placeCharacter(alien, row, col));
+		}
+	}
+	
+	private void generateTank(int amount) {
+		for (int i = 0; i < amount; i++) {
+			Tank alien = new Tank();
+			int row = rand.nextInt(ROWS);
+			int col = rand.nextInt(RANDOM_COLUMN_BOUND) + COLS + 2;
+			alien.setRow(row);
+			alien.setCol(col);
+			alien.setStackPane();
+			
+			model.addAlien(alien);
+			Platform.runLater(() -> model.placeCharacter(alien, row, col));
+		}
+	}
+	
+	private void generateGargantua(int amount) {
+		for (int i = 0; i < amount; i++) {
+			Gargantua alien = new Gargantua();
+			int row = rand.nextInt(ROWS);
+			int col = rand.nextInt(RANDOM_COLUMN_BOUND) + COLS + 2;
 			alien.setRow(row);
 			alien.setCol(col);
 			alien.setStackPane();
@@ -122,18 +301,30 @@ public class Controller {
 	
 	
 	public void pause() {
+		pauseGameTimer();
 		moneyTimeline.pause();
 		alienTimeline.pause();
 	}
 	
 	public void resume() {
+		resumeGameTimer();
 		moneyTimeline.play();
 		alienTimeline.play();
 	}
 	
 	private void animate() {
 		calculateHitsOrDeaths();
+		List<Enemy> aliensToMove = new ArrayList<Enemy>();
 		for (Enemy alien : model.getAliens()) {
+			try {
+				if (model.getDefenderAt(alien.getRow(), alien.getCol()) == null) {
+					aliensToMove.add(alien);
+				}
+			} catch (ArrayIndexOutOfBoundsException e) {
+				aliensToMove.add(alien);
+			}
+		}
+		for (Enemy alien : aliensToMove) {
 			alien.move();
 		}
 		// TODO: Call another method to move bullets
