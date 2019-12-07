e85e481832e3b81ce5cba0c1b1baa27426a5f252
Adrian Bao <adrianbao14@gmail.com>
2019-12-06 21:08:55 -0700

    Update Sprinter animation setup

--- src/characters/Aliens/Sprinter.java
+++ src/characters/Aliens/Sprinter.java
@@ -45,71 +45,32 @@ public class Sprinter extends Enemy {
 	 * animation.
 	 */
 	public void generateAnimations() {
-		// Walk
-		ImageView walkView = new ImageView(SPRINTER_WALK);
-		walkView.setViewport(new Rectangle2D(
-				OFFSET_X, 
-				OFFSET_Y, 
-				SPRINTER_WALK_WIDTH, 
-				SPRINTER_WALK_HEIGHT
-			));
-		walkView.setFitWidth(IMAGE_WIDTH);
-		walkView.setFitHeight(IMAGE_HEIGHT);
-		Animation walkAnimation = new SpriteAnimation(walkView, Duration.millis(SPRINTER_WALK_TIME), 
-				SPRINTER_WALK_COUNT,
+		ImageView walkView = generateAnimation(
+				SPRINTER_WALK, 
+				SPRINTER_WALK_COUNT, 
 				SPRINTER_WALK_COLUMNS, 
-				OFFSET_X, 
-				OFFSET_Y, 
 				SPRINTER_WALK_WIDTH, 
-				SPRINTER_WALK_HEIGHT
-			);
-		walkAnimation.setCycleCount(Animation.INDEFINITE);
-		walkAnimation.play();
-		super.setImageView(walkView); // This will be the initial image view that is shown
-		super.setWalkAnimation(walkAnimation);
-
-		// Attack
-		final ImageView attackView = new ImageView(SPRINTER_ATTACK);
-		attackView.setViewport(new Rectangle2D(
-				OFFSET_X, 
-				OFFSET_Y, 
-				SPRINTER_ATTACK_WIDTH, 
-				SPRINTER_ATTACK_HEIGHT
-			));
-		attackView.setFitWidth(IMAGE_WIDTH);
-		attackView.setFitHeight(IMAGE_HEIGHT);
-		final Animation attackAnimation = new SpriteAnimation(attackView, Duration.millis(SPRINTER_ATTACK_TIME),
+				SPRINTER_WALK_HEIGHT, 
+				SPRINTER_WALK_TIME);
+		super.setWalkView(walkView);
+		
+		ImageView attackView = generateAnimation(
+				SPRINTER_ATTACK, 
 				SPRINTER_ATTACK_COUNT, 
 				SPRINTER_ATTACK_COLUMNS, 
-				OFFSET_X, 
-				OFFSET_Y, 
 				SPRINTER_ATTACK_WIDTH, 
-				SPRINTER_ATTACK_HEIGHT
-			);
-		attackAnimation.setCycleCount(Animation.INDEFINITE);
-		attackAnimation.play();
-		super.setAttackAnimation(attackAnimation);
-
-		// Die
-		final ImageView dieView = new ImageView(SPRINTER_DIE);
-		dieView.setViewport(new Rectangle2D(
-				OFFSET_X, 
-				OFFSET_Y, 
-				SPRINTER_DIE_WIDTH, 
-				SPRINTER_DIE_HEIGHT));
-		dieView.setFitWidth(IMAGE_WIDTH);
-		dieView.setFitHeight(IMAGE_HEIGHT);
-		final Animation dieAnimation = new SpriteAnimation(dieView, Duration.millis(SPRINTER_DIE_TIME), 
-				SPRINTER_DIE_COUNT,
+				SPRINTER_ATTACK_HEIGHT, 
+				SPRINTER_ATTACK_TIME);
+		super.setAttackView(attackView);
+		
+		ImageView dieView = generateAnimation(
+				SPRINTER_DIE, 
+				SPRINTER_DIE_COUNT, 
 				SPRINTER_DIE_COLUMNS, 
-				OFFSET_X, 
-				OFFSET_Y, 
 				SPRINTER_DIE_WIDTH, 
-				SPRINTER_DIE_HEIGHT
-			);
-		dieAnimation.setCycleCount(Animation.INDEFINITE);
-		dieAnimation.play();
-		super.setDieAnimation(dieAnimation);
+				SPRINTER_DIE_HEIGHT, 
+				SPRINTER_DIE_TIME);
+		super.setDieView(dieView);
 	}
 
 }
