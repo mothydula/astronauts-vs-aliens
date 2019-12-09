e29f92b9fb3a9c6deab77dc63291bd67ed8869a5
Timothy <timothylukau@email.arizona.edu>
2019-12-08 17:52:37 -0700

    Implementing threading conditions to testing

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -11,14 +11,32 @@ import javafx.application.Application;
 
 class Testing {
 	private static Thread t;
+	private static Thread t2;
 	private Model testModel = new Model();
 	private Controller testController = new Controller(testModel);
+	
 	@BeforeClass
 	public static void setUpClass() throws InterruptedException {
 		// Initialise Java FX
 
 		System.out.printf("About to launch FX App\n");
 		t = new Thread("JavaFX Init Thread") {
+			public void run() {
+				Application.launch(View.class, new String[0]);
+			}
+		};
+		t.setDaemon(true);
+		t.start();
+		System.out.printf("FX App thread started\n");
+		Thread.sleep(500);
+	}
+	
+	@BeforeClass
+	public static void setUpClass2() throws InterruptedException {
+		// Initialise Java FX
+
+		System.out.printf("About to launch FX App\n");
+		t2 = new Thread("JavaFX Init Thread") {
 			public void run() {
 				Application.launch(View.class, new String[0]);
 			}
@@ -126,9 +144,32 @@ class Testing {
 		testControllerTwo.increaseSpeed();
 		assertEquals(2, testControllerTwo.getSpeedMultiplier());
 		
-		testModelTwo.setWaveNumber(2);
+		assertEquals(0, testModelTwo.getAliens().size());
 		
+		testModelTwo.setWaveNumber(1);
 		testControllerTwo.firstWave.run();
+		testControllerTwo.firstWave.cancel();
+		testControllerTwo.secondWave.run();
+		testControllerTwo.secondWave.cancel();
+		
+		testControllerTwo.placeCharacter(moneyTree, 0, 0);
+		
+		//Remove the tile
+		testModel.removeTower(moneyTree, 0, 0);
+		
+		assertNotEquals(0, testModelTwo.getAliens().size());
+		
+		
+		
+		
+		testModelTwo.setWaveNumber(2);
+		testControllerTwo.firstWave.run();
+		testControllerTwo.secondWave.run();
+	}
+	
+	@Test
+	void enemyTests(){
+		
 	}
 	
 
